b926a5324dcb48065d0a3d0a3346ecc04e565810
Janicki, Joseph John <jjanicki3@gatech.edu>
2020-11-19 22:37:41 -0500

    update build process to prepare repo zip for google drive upload

--- build_submission_and_demo.py
+++ build_submission_and_demo.py
@@ -4,7 +4,12 @@ import shutil
 import pandas as pd
 import geopandas as gp
 
-
+"""
+This script is used to build
+(1) The Canvas submission folder (with all data removed)
+(2) The pared down version of the repo with data limited to 1 cluster + the docker app build
+(3) The full repo that is uploaded to Google Drive that includes all of the data. 
+"""
 
 ################################################################################
 # Prepare canvas submission of just code + docs
@@ -35,7 +40,7 @@ os.makedirs(CODE_DEST)
 os.makedirs(DOC_DEST)
 
 # copy relevant folders
-for folder in ('app', 'data', 'docs', 'model'):
+for folder in ('app', 'data', 'docs', 'model', 'raw_data'):
     shutil.copytree(folder, os.path.join(CODE_DEST, folder))
 
 # copy relevant files
@@ -48,7 +53,7 @@ shutil.copy('README.txt', DEST)
 # delete unnecessary files/folders. delete data sets.
 for item in ('.DS_Store', '__pycache__', '.vscode', '.ipynb_checkpoints',
              'source-data', 'Final_Visualization_Input_Data.zip', 
-             '*.gz', '*.pkl', '*.csv', '*.obj'):
+             '*.gz', '*.pkl', '*.csv', '*.obj', '*.csv', '*.json', '*.zip'):
     os.system('find {} -name {} | xargs rm -rf'.format(DEST, item))
 
 print('Done with submission folder.')
@@ -167,6 +172,40 @@ print('Done with demo app.')
 
 
 
+
+################################################################################
+# Prepare canvas submission of just code + docs
+################################################################################
+
+################################################################################
+################################################################################
+DEST = './00_REPO_GDRIVE_UPLOAD/team046repo/'
+################################################################################
+################################################################################
+
+# If destination directory exists, prompt if OK to delete
+# If not OK, exit
+if os.path.exists(DEST):
+    print('\n\n\nDestination already exists: {}. '.format(DEST))
+    resp = input('Okay to delete? y/[n]: ').strip().lower()
+    if resp not in ('y', 'yes'):
+        print('Exiting...')
+        sys.exit(1)
+    # delete existing directory and start from scratch    
+    os.system('rm -rf {}'.format(DEST))
+
+# make destination directory
+os.makedirs(DEST)
+
+# copy relevant folders
+for folder in ('app', 'data', 'docs', 'model', 'raw_data'):
+    shutil.copytree(folder, os.path.join(CODE_DEST, folder))
+
+# copy relevant files
+for f in ('README.md', 'requirements.txt', 'Dockerfile', 'boot.sh', 'environment.yml', 'build_submission_and_demo.py'):
+    shutil.copy(f, CODE_DEST)
+
+os.system('zip -r {}.zip {}'.format(DEST, DEST))
 
 
 
